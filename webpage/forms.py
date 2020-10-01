27aada040d906b87f974a17440c5efbbe3a7ebf9
Tiago Rodrigues <rodriguest2@yahoo.com>
2020-09-30 20:50:36 -0500

    should work

--- webpage/forms.py
+++ webpage/forms.py
@@ -9,12 +9,6 @@ class PostForm(forms.ModelForm):
         model=Post
         fields=('title','category','body','post_img')
 
-        '''cat_choices = Category.objects.all().values_list('name','name')
-        cat_list = []
-
-        for item in cat_choices:
-            cat_list.append(item)'''
-
         widgets={
             'title':forms.TextInput(attrs={
                 'class':'form-control',
